9ddd3fd6804403a6336364730ac7990eaba15508
klishevich <busation@gmail.com>
2016-03-26 22:39:43 +0300

    debug login problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,29 @@ class ApplicationController < ActionController::Base
 
   def authentication
     logout if enter_from_different_shop?
+    Rails.logger.info('start authentication log')
+    Rails.logger.info('api_key')
+    Rails.logger.info(Figaro.env.insales_api_key)
+    Rails.logger.info('api_secret')
+    Rails.logger.info(Figaro.env.insales_api_secret)
+    Rails.logger.info('api_autologin_url')
+    Rails.logger.info(Figaro.env.insales_api_autologin_url)
 
     if current_app and current_app.authorized?
+      Rails.logger.info('if current_app and current_app.authorized?')
+      acc = Account.find_by(insales_subdomain: current_app.shop)
+      Rails.logger.info('acc')
+      Rails.logger.info(acc.insales_subdomain) if acc
       return if @account = Account.find_by(insales_subdomain: current_app.shop)
     end
 
     store_location
 
     if account_by_params
+      Rails.logger.info('account_by_params')
       init_authorization account_by_params
     else
+      Rails.logger.info('no account_by_params')
       redirect_to login_path
     end
   end

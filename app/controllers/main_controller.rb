743afc4554537f6d22dbccf29ed51f5bbcc5ceb1
klishevich <busation@gmail.com>
2016-03-27 22:48:01 +0300

    debug

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,12 +10,12 @@ class MainController < ApplicationController
   	my_url = "http://" + my_subdomain + "/admin/products.xml"
 	Rails.logger.info( 'my_url:' )
 	Rails.logger.info(my_url)
-  	xml_data = %{<?xml version="1.0" encoding="UTF-8"?>}
+  	# xml_data = %{<?xml version="1.0" encoding="UTF-8"?>}
 	uri = URI.parse(my_url)
 	request = Net::HTTP::Get.new uri.path
-	request.body = xml_data
-	request.content_type = 'text/xml'
-	request.basic_auth 'mrjones', my_pass
+	# request.body = xml_data
+	# request.content_type = 'text/xml'
+	# request.basic_auth 'mrjones', my_pass
 	response = Net::HTTP.start(uri.host, uri.port) {|http| http.request(request)}
 	Rails.logger.info( 'response body:' )
 	Rails.logger.info(response.body)

c2f5231f2777b36d03ebe495069dffc91c16d4d6
klishevich <busation@gmail.com>
2016-04-20 00:31:47 +0300

    update product_field_values methods

--- app/models/seo_filters_update.rb
+++ app/models/seo_filters_update.rb
@@ -36,6 +36,18 @@ class SeoFiltersUpdate
 		# Rails.logger.info(temp_products)
 	end
 
+	def get_product(id)
+		Rails.logger.info("------- 1) start get_product -------")
+		url = "http://" + subdomain + "/admin/products/" + id.to_s + ".json"
+		uri = URI.parse(url)
+		request = Net::HTTP::Get.new (uri.path)
+		request.basic_auth module_name, pass
+		response = Net::HTTP.start(uri.host, uri.port) {|http| http.request(request)}
+		parse_product = JSON.parse(response.body)
+		Rails.logger.info(parse_product.to_json)
+		Rails.logger.info("------- 1) finish get_product -------")
+	end
+
 	def get_seofilters
 		Rails.logger.info("------- 2) start get_seofilters -------")
 		collection_filters = get_collection_filters
@@ -70,6 +82,28 @@ class SeoFiltersUpdate
 		# Rails.logger.info(@products_links)
 	end
 
+	def calc_products_links2
+		Rails.logger.info("------- 3) start calc_products_links2 -------")
+		products_links = Array.new
+		@products.each do |product|
+			#hardcode seo product_field_id = 31206
+			product_seo = product["product_field_values"].select {|pfv| pfv["product_field_id"] == 31206 }.first
+			product_seo ||= ""
+			plinks = calc_product_links2(product)
+			if (product_seo != plinks)
+				temp_hash = Hash.new
+				temp_hash["product_id"] = product["id"]
+				temp_hash["product_title"] = product["title"]
+				temp_hash["product_links"] = plinks
+				temp_hash["need_update"] = true
+				products_links.push(temp_hash)
+			end	
+		end
+		@products_links = products_links
+		Rails.logger.info("------- 3) finish calc_products_links2 @products_links.count #{@products_links.count} -------")
+		# Rails.logger.info(@products_links)
+	end
+
 	def put_product_by_index(ar_ind)
 		ar_index = ar_ind.to_i
 		product_id = @products_links[ar_index]["product_id"]
@@ -94,6 +128,37 @@ class SeoFiltersUpdate
 		res
 	end
 
+	def put_product_by_index2(ar_ind)
+		ar_index = ar_ind.to_i
+		product_id = @products_links[ar_index]["product_id"]
+		product_links = @products_links[ar_index]["product_links"]
+		my_subdomain = @account.insales_subdomain
+		my_pass = @account.password
+		my_url = "http://" + my_subdomain + "/admin/products/" + product_id.to_s + ".json"
+		json_data = {
+			"id" => product_id,
+			"product_field_values" => [
+				{
+					"product_field_id" => 31206,
+					"value" => product_links 
+				}
+			]
+			}.to_json
+		uri = URI.parse(my_url)
+		request = Net::HTTP::Put.new uri.path
+		request.body = json_data
+		request.content_type = 'application/json'
+		request.basic_auth 'mrjones', my_pass
+		response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+		res = JSON.parse(response.body)
+		Rails.logger.info("------- 4) put_product_by_index -------")
+		Rails.logger.info('request.body')
+		Rails.logger.info(request.body)
+		Rails.logger.info('response.body')
+		Rails.logger.info(res)
+		res
+	end
+
 	def put_all_products
 		updated_products = Array.new
 		@products_links.each_with_index do |product, index|
@@ -114,6 +179,24 @@ class SeoFiltersUpdate
 	private
 
 	def calc_product_links(product)
+		product_a_link = ""
+		@seofilters.each do |sf|
+			collection_id = sf["collection_id"]
+			property_id = sf["property_id"]
+			property_value = sf["property_value"]
+			if (product["collections_ids"].include? collection_id) && 
+				(product["characteristics"].select {|ch| ch["property_id"] == property_id && ch["title"] == property_value}.count > 0)
+				product_url = sf["url"]
+				product_link_text = sf["link_text"]
+				product_a_link += "<span><a href=#{product_url}>#{product_link_text}</a>&nbsp;&nbsp;&nbsp;&nbsp;</span>"
+			end
+		end
+		# Rails.logger.info("------- calc_product_links product_a_link #{product["title"]} #{product["id"]} #{product_a_link} -------")
+		# Rails.logger.info(@product_links)
+		return product_a_link
+	end
+
+	def calc_product_links2(product)
 		product_a_link = ""
 		@seofilters.each do |sf|
 			collection_id = sf["collection_id"]
